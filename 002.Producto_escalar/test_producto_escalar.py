111951fe5aee0de210ea2f81f9bfb9f76faca222
Gonzalo Déniz Acosta <gdeniz@inerza.com>
2023-06-06 19:42:22 +0100

    Inserta nuevos test

--- 002.Producto_escalar/test_producto_escalar.py
+++ 002.Producto_escalar/test_producto_escalar.py
@@ -1,3 +1,4 @@
+import pytest
 from producto_escalar import *
 
 
@@ -20,14 +21,48 @@ def test_producto_escalar_3() -> None:
     experado: int = 18
     assert producto_escalar(u, v) == experado
 
+
 def test_producto_escalar_4() -> None:
     u: tuple[int, ...] = (10, -4)
     v: tuple[int, ...] = (1, 0)
     experado: int = 10
     assert producto_escalar(u, v) == experado
 
+
 def test_producto_escalar_5() -> None:
         u: tuple[int, ...] = (10,)
         v: tuple[int, ...] = (1,)
         experado: int = 10
         assert producto_escalar(u, v) == experado
+
+
+def test_producto_escalar_igual_longitud() -> None:
+    u = (1, 2, 3)
+    v = (4, 5, 6)
+    assert producto_escalar(u, v) == 32  # 1*4 + 2*5 + 3*6 = 32
+
+
+def test_producto_escalar_longitud_diferente() -> None:
+    u = (1, 2, 3)
+    v = (4, 5)
+    with pytest.raises(ValueError) as e:
+        producto_escalar(u, v)
+    assert str(e.value) == "El número de componente debe ser iguales en los dos vectores"
+
+
+def test_producto_escalar_vacio() -> None:
+    u = ()
+    v = ()
+    assert producto_escalar(u, v) == 0  # El producto escalar de dos vectores vacíos debería ser 0
+
+
+def test_producto_escalar_negativos() -> None:
+    u = (-1, -2, -3)
+    v = (-4, -5, -6)
+    assert producto_escalar(u, v) == 32  # El producto escalar de dos vectores negativos es positivo
+
+
+def test_producto_escalar_cero() -> None:
+    u = (0, 0, 0)
+    v = (4, 5, 6)
+    assert producto_escalar(u, v) == 0  # El producto escalar con un vector de ceros siempre debería ser cero
