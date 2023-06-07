6f514fdd42bce77bae517ae7d9b51e874e00cbc9
Gonzalo Déniz Acosta <gdeniz@inerza.com>
2023-06-07 16:32:44 +0100

    003.Elemento maximo matriz

---
+++ 003.Elemento_maximo_matriz/README.md
@@ -0,0 +1,7 @@
+# 002 Elemento máximo de una matriz
+Desarrolle un procedimiento para obtener el elemento máximo de una matriz de ristras y su posición.
+
+## Fuente del problema
+Apuntes de la asignatura MP1 de la ULPGC. Capítulo Metodologías de Desarrollo de Programas. Página 60, ejercicio 2.
+
+
---
+++ 003.Elemento_maximo_matriz/elemento_maximo_matriz.py
@@ -0,0 +1,33 @@
+"""
+Desarrolle un procedimiento para obtener el elemento máximo de una matriz de ristras y su posición..
+"""
+
+
+def elemento_maximo_matriz(matriz: list[list[int]]) -> dict[str, int | tuple[int, int]]:
+
+    maximo: int = matriz[0][0]
+    posicion: tuple[int, int] = (0, 0)
+
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            if matriz[i][j] > maximo:
+                maximo = matriz[i][j]
+                posicion = (i, j)
+
+    return {'maximo': maximo,
+            'posicion': posicion}
+
+
+def main() -> None:
+    matriz: list[list[int]] = [
+        [1, 2, 3],
+        [4, 9, 6],
+        [7, 8, 5]
+    ]
+
+    resultado: dict[str, int | tuple[int, int]] = elemento_maximo_matriz(matriz)
+    print(str(resultado))
+
+
+if __name__ == "__main__":
+    main()
