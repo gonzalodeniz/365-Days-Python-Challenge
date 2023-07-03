c35347eff9c5ee24086c5a7397549bf2d6e2ae4c
Gonzalo Déniz Acosta <gdeniz@inerza.com>
2023-07-03 17:26:31 +0100

    Refactoriza 012 Matriz ordenado por proximidad

--- 012.MatricesOrdenadosPorProximidad/test_matriz_ordenado_por_proximidad.py
+++ 012.MatricesOrdenadosPorProximidad/test_matriz_ordenado_por_proximidad.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import pytest
 from matriz_ordenado_por_proximidad import MatrizOrdenadoProximidad
 
 
@@ -11,12 +10,14 @@ def test_matriz_ordenado_proximidad_init() -> None:
     assert matriz.orden == orden, f"El orden esperado es {orden}, pero se obtuvo {matriz.orden}"
     assert matriz.m.toList() == [[1, 2, 3], [2, 3, 1], [3, 2, 1]]
 
+
 def test_inicializa_fila() -> None:
     orden = 5
     matriz = MatrizOrdenadoProximidad(orden)
     assert matriz._inicializa_fila() == [1, 2, 3, 4, 5]
 
-def test_es_mas_proximo_a_que_b()  -> None:
+
+def test_es_mas_proximo_a_que_b() -> None:
     assert MatrizOrdenadoProximidad._es_mas_proximo_a_que_b(1, 2, 1) == True
     assert MatrizOrdenadoProximidad._es_mas_proximo_a_que_b(2, 1, 1) == False
     assert MatrizOrdenadoProximidad._es_mas_proximo_a_que_b(1, 2, 2) == False
@@ -34,12 +35,10 @@ def test_esta_fila_ordenada_por_proximidad() -> None:
     assert MatrizOrdenadoProximidad._esta_fila_ordenada_por_proximidad([5, 4, 3, 2, 1], 1) == False
     assert MatrizOrdenadoProximidad._esta_fila_ordenada_por_proximidad([5, 4, 3, 2, 1], 5) == True
 
+
 def test_crea_fila_proximidad() -> None:
     orden = 5
     matriz = MatrizOrdenadoProximidad(orden)
     assert matriz._crea_fila_proximidad(1) == [1, 2, 3, 4, 5]
     assert matriz._crea_fila_proximidad(3) == [3, 4, 2, 5, 1]
     assert matriz._crea_fila_proximidad(5) == [5, 4, 3, 2, 1]
-
-
-
